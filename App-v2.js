4e829422929ce663ef8546da5c5c0f3283da79cf
Natnael Endale <n87294578@gmail.com>
2025-12-13 19:12:05 +0300

    cleaning the temp data

--- App-v2.js
+++ App-v2.js
@@ -3,17 +3,7 @@ import StarRating from "./StarRating";
 
 
 
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
+
   {
     imdbID: "tt0088763",
     Title: "Back to the Future",
