ce50e2e3455ef8c1765f57ead47ca1f9bfdf4ad3
Natnael Endale <n87294578@gmail.com>
2025-12-15 21:49:44 +0300

    arranging the reducer function

--- App-v1.js
+++ App-v1.js
@@ -1,12 +1,5 @@
 import { useState } from "react";
 
-
-
-
-
-const average = (arr) =>
-  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
-
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
