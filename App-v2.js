8c9010a157ef27187af95613dd5c4f0d30ef146a
Natnael Endale <n87294578@gmail.com>
2025-12-21 21:40:20 +0300

    Remove movie fetching logic from App component
    Removed movie fetching logic and cleanup code.

--- App-v2.js
+++ App-v2.js
@@ -1,41 +1,5 @@
 
 export default function App() {
- 
-  
-
- 
-          const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-            { signal: controller.signal }
-          );
-
-          if (!res.ok)
-            throw new Error("Something went wrong with fetching movies");
-
-          const data = await res.json();
-          if (data.Response === "False") throw new Error("Movie not found");
-
-          setMovies(data.Search);
-          setError("");
-        } catch (err) {
-          if (err.name !== "AbortError") {
-            console.log(err.message);
-            setError(err.message);
-          }
-        } finally {
-          setIsLoading(false);
-        }
-      }
-
-     loseMovie();
-      fetchMovies();
-
-      return function () {
-        controller.abort();
-      };
-    },
-    [query]
-  );
 
   return (
     <>
